a93dc70ca64333bcc6341d21729bf6e0f74a047c
Jason Womack <jasonwomack@Jasons-MacBook-Air.local>
2022-03-24 17:18:46 -0500

    api schema product work

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,9 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/products', function(){
-    return 'products';
+    return Product::all();
     });
 
+    Route::post('/products', function(){
+        return Product::create([
+            'name'=>'Product One',
+            'slug' => 'product-one',
+            'description' => 'This is prodcut one',
+            'price' => '55.55'
+        ]);
+        });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
