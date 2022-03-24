812c5cdaefa3ff4f3358581783477b65be0d3850
Jason Womack <jasonwomack@Jasons-MacBook-Air.local>
2022-03-24 17:50:13 -0500

    postman work routing controller work

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,18 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/products', function(){
-    return Product::all();
-    });
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'store']);
 
-    Route::post('/products', function(){
-        return Product::create([
-            'name'=>'Product One',
-            'slug' => 'product-one',
-            'description' => 'This is prodcut one',
-            'price' => '55.55'
-        ]);
-        });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
